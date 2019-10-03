d090ad3fd3da06522c1f153b8a6cca98ac7b5b49
Ivan Akhlestin <i.akhlestin@rambler-co.ru>
2019-10-04 00:52:01 +0300

    wip

--- src/functions.rs
+++ src/functions.rs
@@ -1,22 +1,34 @@
 
-trait ObjFunction {
-    fn predTransformVec(preds: Vec<f64>) -> Vec<f64>;
-    fn predTransform(pred: f64) -> f64;
+pub enum FunctionType {
+    RankPairwise,
+    BinaryLogistic,
+    BinaryLogitraw,
+    MultiSoftmax,
+    MultiSoftprob,
+    RegLinear,
+    RegLogistic,
+}
+
+pub trait ObjFunction {
+    fn pred_transform_vec(preds: Vec<f64>) -> Vec<f64>;
+    fn pred_pransform(pred: f64) -> f64;
     fn sigmoid(x: f64) -> f64 {
         1f64 / (1f64 + (-x).exp())
     }
+
+    fn from_type(tp: FunctionTypes) -> Box<ObjFunction> {
+
+    }
 }
-
-
 
 struct RegLossObjLogistic {}
 
 impl ObjFunction for RegLossObjLogistic {
     /// Logistic regression.
-    fn predTransformVec(preds: Vec<f64>) -> Vec<f64> {
+    fn pred_transform_vec(preds: Vec<f64>) -> Vec<f64> {
         return preds.into_iter().map(RegLossObjLogistic::sigmoid).collect();
     }
-    fn predTransform(pred: f64) -> f64 {
+    fn pred_pransform(pred: f64) -> f64 {
         RegLossObjLogistic::sigmoid(pred)
     }
 }
@@ -25,11 +37,19 @@ struct SoftmaxMultiClassObjClassify {}
 
 impl ObjFunction for SoftmaxMultiClassObjClassify {
     /// Multiclass classification.
-    fn predTransformVec(preds: Vec<f64>) -> Vec<f64> {
-        let (maxIndex, max) = preds.iter().enumerate().max_by(|(i1, x1), (i2, x2)| x1.partial_cmp(x2).unwrap()).unwrap();
-        return vec![maxIndex as f64;1]
+    fn pred_transform_vec(preds: Vec<f64>) -> Vec<f64> {
+        match preds.get(0) {
+            Option::Some(init) => {
+                let (max_index, _max) = preds.iter().enumerate()
+                    .fold((0, init), |(i1, v1), (i2, v2)|
+                        if v1 >= v2 { (i1, v1) } else { (i2, v2) });
+                return vec![max_index as f64; 1]
+            }
+            // empty vector
+            Option::None => preds
+        }
     }
-    fn predTransform(pred: f64) -> f64 {
+    fn pred_pransform(_pred: f64) -> f64 {
         std::unimplemented!();
     }
 }
@@ -38,13 +58,18 @@ struct SoftmaxMultiClassObjProb {}
 
 impl ObjFunction for SoftmaxMultiClassObjProb {
     ///  Multiclass classification (predicted probability).
-    fn predTransformVec(preds: Vec<f64>) -> Vec<f64> {
-        let max = preds.iter().fold(preds.get(0), |a, b| b.max(*a.unwrap())).unwrap();
-        let sum = preds.iter().map(|x| (x - max).exp()).sum();
-        return preds.iter().map(|x| (x - max).exp()/sum).collect();
+    fn pred_transform_vec(preds: Vec<f64>) -> Vec<f64> {
+        match preds.get(0) {
+            Option::Some(init) => {
+                let max = preds.iter().fold(*init, |a, b| b.max(a));
+                let sum: f64 = preds.iter().map(|x| (x - max).exp()).sum();
+                return preds.iter().map(|x| (x - max).exp() / sum).collect();
+            }
+            // empty vector
+            Option::None => preds
+        }
     }
-    fn predTransform(pred: f64) -> f64 {
+    fn pred_pransform(_pred: f64) -> f64 {
         std::unimplemented!();
     }
 }
-
---
+++ src/predictor.rs
@@ -0,0 +1,10 @@
+use self::super::functions::ObjFunction;
+use std::fs::File;
+
+struct Predictor {
+    obj_function: dyn ObjFunction,
+}
+
+impl Predictor {
+
+}
