e4d00268c9e488fdf098895fe19ef753431191be
Akhlestin Ivan <i.akhlestin@rambler-co.ru>
2019-10-07 18:43:29 +0300

    RegTree rewriting

--- src/model_reader.rs
+++ src/model_reader.rs
@@ -1,68 +1,39 @@
 use byteorder::{BE, LE, ReadBytesExt};
+use std::result::Result;
+use std::io::Error;
 
-impl ModelReader for ReadBytesExt {
-    #[inline]
-    fn read_4bytes(&mut self) -> [u8; 4] {
-        let mut buffer = [0u8; 4];
-        self.read_exact(&mut buffer[..]).expect("Something went wrong reading the file");
-        return buffer;
+pub type ModelReadResult<T> = Result<T, Error>;
+
+
+pub trait ModelReader: ReadBytesExt {
+    fn read_i32_le(&mut self) -> ModelReadResult<i32> {
+        return self.read_i32::<LE>();
     }
 
-    fn read_8bytes(&mut self) -> [u8; 8] {
-        let mut buffer = [0u8; 8];
-        self.read_exact(&mut buffer[..]).expect("Something went wrong reading the file");
-        return buffer;
+    fn read_float_le(&mut self) -> ModelReadResult<f32> {
+        return self.read_f32::<LE>();
     }
 
-    fn read_byte_as_int(&mut self) -> i32 {
-        let mut buffer = [0u8; 4];
-        self.read(&mut buffer[..1]).expect("Something went wrong reading the file");
-        return i32::from_le_bytes(buffer);
+    fn read_byte_as_int(&mut self) -> ModelReadResult<i32> {
+        let byte = self.read_u8()?;
+        return Ok(byte as i32);
     }
 
-    fn read_int_be(&mut self) -> i32 {
-        return i32::from_be_bytes(self.read_4bytes());
+    fn read_int_vec(&mut self, num_values: usize) -> ModelReadResult<Vec<i32>> {
+        return (0..num_values).map(|_| self.read_i32_le()).collect();
     }
 
-    fn read_int_le(&mut self) -> i32 {
-        return i32::from_le_bytes(self.read_4bytes());
-    }
-
-    fn read_int_array(&mut self, num_values: usize) -> Vec<i32> {
-        return (0..num_values).map(|_| self.read_int_le()).collect();
-    }
-
-    fn read_uint_le(&mut self) -> u32 {
-        return u32::from_le_bytes(self.read_4bytes());
-    }
-
-    fn read_long_le(&mut self) -> i64 {
-        return i64::from_le_bytes(self.read_8bytes());
-    }
-
-    fn read_float_le(&mut self) -> f32 {
-        return f32::from_le_bytes(self.read_4bytes());
-    }
-
-    fn read_float_array(&mut self, num_values: usize) -> Vec<f32> {
+    fn read_float_vec(&mut self, num_values: usize) -> ModelReadResult<Vec<f32>> {
         return (0..num_values).map(|_| self.read_float_le()).collect();
     }
 
-    fn read_double_array_be(&mut self, num_values: usize) -> Vec<f64> {
-        return (0..num_values).map(|_| f64::from_be_bytes(self.read_8bytes())).collect();
+    fn read_double_vec_be(&mut self, num_values: usize) -> ModelReadResult<Vec<f64>> {
+        return (0..num_values).map(|_| self.read_f64::<BE>()).collect();
     }
 
-    fn skip(&mut self, num_bytes: usize) {
-        unimplemented!()
-    }
-
-    fn read_string_with_len(&mut self, num_bytes: usize) -> String {
-        unimplemented!()
-    }
-
-    fn read_string(&mut self) -> String {
-        let len = self.read_long_le() as usize;
-        return self.read_string_with_len(len);
+    fn skip(&mut self, num_bytes: usize) -> ModelReadResult<()> {
+        let mut vec: Vec<u8> = Vec::with_capacity(num_bytes);
+        return self.read_exact(vec.as_mut_slice());
     }
 }
 
@@ -70,8 +41,6 @@ impl ModelReader for ReadBytesExt {
 mod tests {
     #[test]
     fn it_works() {
-        let mut buf = [1u8, 0, 0, 0];
-        let i = i32::from_le_bytes(buf);
-        assert_eq!(i, 1);
+
     }
 }
--- src/regtree.rs
+++ src/regtree.rs
@@ -1,4 +1,6 @@
-use crate::model_reader::ModelReader;
+use crate::model_reader::{ModelReader, ModelReadResult};
+use std::f32;
+
 
 struct Param {
     /// number of start root
@@ -18,16 +20,104 @@ struct Param {
 }
 
 impl Param {
-    fn new<T: ModelReader>(reader: &mut T) -> Param {
-        return Param {
-            num_roots: reader.read_int_le(),
-            num_nodes: reader.read_int_le(),
-            num_deleted: reader.read_int_le(),
-            max_depth: reader.read_int_le(),
-            num_feature: reader.read_int_le(),
-            size_leaf_vector: reader.read_int_le(),
-            reserved: reader.read_int_array(31),
+    fn new<T: ModelReader>(reader: &mut T) -> ModelReadResult<Param> {
+        return Ok(Param {
+            num_roots: reader.read_i32_le()?,
+            num_nodes: reader.read_i32_le()?,
+            num_deleted: reader.read_i32_le()?,
+            max_depth: reader.read_i32_le()?,
+            num_feature: reader.read_i32_le()?,
+            size_leaf_vector: reader.read_i32_le()?,
+            reserved: reader.read_int_vec(31)?,
+        })
+    }
+}
 
-        }
+
+struct Node {
+    /// pointer to parent, highest bit is used to indicate whether it's a left child or not
+    parent: i32,
+    /// pointer to right
+    cleft: i32,
+    /// pointer to right
+    cright: i32,
+    /// split feature index, left split or right split depends on the highest bit
+    leaf_value: f32,
+    split_cond: f32,
+    default_next: i32,
+    split_index: i32,
+    is_leaf: bool,
+}
+
+impl Node {
+    fn is_default_left(sindex: i32) -> bool {
+        return (sindex >> 31) != 0;
+    }
+
+    fn decode_split_index(sindex: i32) -> i32 {
+        return ((sindex as i64) & ((1i64 << 31) - 1i64)) as i32;
+    }
+
+    fn new<T: ModelReader>(reader: &mut T) -> ModelReadResult<Node> {
+        let (parent, cleft, cright, sindex) =
+            (reader.read_i32_le()?, reader.read_i32_le()?, reader.read_i32_le()?, reader.read_i32_le()?);
+        let is_leaf = cleft == -1;
+
+        let leaf_value = if is_leaf {reader.read_float_le()?} else { f32::NAN };
+        let split_cond = if is_leaf { f32::NAN } else {reader.read_float_le()?};
+
+        let split_index = Node::decode_split_index(sindex);
+        let default_next = if Node::is_default_left(split_index) {cleft} else {cright};
+
+        return Ok(Node {
+            parent: parent,
+            cleft: cleft,
+            cright: cright,
+            leaf_value: leaf_value,
+            split_cond: split_cond,
+            default_next: default_next,
+            split_index: split_index,
+            is_leaf: is_leaf,
+        })
+
+    }
+}
+
+
+struct RTreeNodeStat {
+    /// loss chg caused by current split
+    loss_chg: f32,
+    /// sum of hessian values, used to measure coverage of data
+    sum_hess: f32,
+    /// weight of current node
+    base_weight: f32,
+    /// number of child that is leaf node known up to now
+    leaf_child_cnt: i32,
+}
+
+impl RTreeNodeStat {
+    fn new<T: ModelReader>(reader: &mut T) -> ModelReadResult<RTreeNodeStat> {
+        return Ok(RTreeNodeStat{
+            loss_chg: reader.read_float_le()?,
+            sum_hess: reader.read_float_le()?,
+            base_weight: reader.read_float_le()?,
+            leaf_child_cnt: reader.read_i32_le()?,
+        })
+    }
+}
+
+/// Regression tree
+pub struct RegTree {
+    param: Param,
+    nodes: Vec<Node>,
+    stats: Vec<RTreeNodeStat>,
+}
+
+impl RegTree {
+    fn new<T: ModelReader>(reader: &mut T) -> ModelReadResult<RegTree> {
+        let param = Param::new(reader)?;
+        let nodes: ModelReadResult<Vec<Node>> = (0..param.num_nodes).map(|_| Node::new(reader)).collect();
+        let stats: ModelReadResult<Vec<RTreeNodeStat>> = (0..param.num_nodes).map(|_| RTreeNodeStat::new(reader)).collect();
+        return Ok(RegTree{param, nodes: nodes?, stats: stats?});
     }
 }
